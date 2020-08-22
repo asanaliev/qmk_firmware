5ac55f8327f856faccd6e5af20bd7d075b99342b
Askat Asanaliev <aasanaliev@gmail.com>
2020-08-23 00:28:44 +0100

    Custom layouts

--- keyboards/handwired/dactyl_manuform/5x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/rules.mk
@@ -17,4 +17,8 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+
+AUTO_SHIFT_ENABLE = yes
+LEADER_ENABLE = yes
